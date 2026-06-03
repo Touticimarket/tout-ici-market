5eb03ea12c09a94f3fbf4e9abcd0daf4ac6d5b14
Touticimarket <contact@touticimarket.fr>
2026-06-03 17:44:33 +0200

    Delete Categories.tsx

--- Categories.tsx
+++
@@ -1,5 +0,0 @@
-import Header from "@/components/Header";
-import PopularListings from "@/components/PopularListings";
-export default function Annonces() {
-  return <main><Header/><div className="mx-auto max-w-7xl px-4 md:px-8 py-10"><h1 className="text-4xl font-black mb-8">Toutes les annonces</h1></div><PopularListings/></main>;
-}
