019dee276ae449a8f70a911ecee8df1fbc05cd16
Touticimarket <contact@touticimarket.fr>
2026-06-05 17:07:58 +0200

    Refactor links into an array for dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 export default function Home() {
+  const links = [
+    { href: "/automobile", left: "6%", top: "51%" },
+    { href: "/moto", left: "21%", top: "51%" },
+    { href: "/immobilier", left: "36%", top: "51%" },
+    { href: "/emploi", left: "51%", top: "51%" },
+    { href: "/telephones", left: "66%", top: "51%" },
+
+    { href: "/maison-jardin", left: "6%", top: "61%" },
+    { href: "/vacances-loisirs", left: "21%", top: "61%" },
+    { href: "/bricolage", left: "36%", top: "61%" },
+    { href: "/jeux-consoles", left: "51%", top: "61%" },
+    { href: "/autres", left: "66%", top: "61%" },
+  ];
+
   return (
-    <main style={{ position: "relative" }}>
+    <main style={{ position: "relative", width: "100%" }}>
       <img
         src="/public/banner-home.png"
         alt="TOUT ICI Market"
@@ -10,44 +24,21 @@ export default function Home() {
         }}
       />
 
-      <a
-        href="/automobile"
-        title="Automobile"
-        style={{
-          position: "absolute",
-          left: "4%",
-          top: "41%",
-          width: "17%",
-          height: "8%",
-          display: "block",
-        }}
-      />
-
-      <a
-        href="/moto"
-        title="Moto"
-        style={{
-          position: "absolute",
-          left: "22%",
-          top: "41%",
-          width: "17%",
-          height: "8%",
-          display: "block",
-        }}
-      />
-
-      <a
-        href="/immobilier"
-        title="Immobilier"
-        style={{
-          position: "absolute",
-          left: "40%",
-          top: "41%",
-          width: "17%",
-          height: "8%",
-          display: "block",
-        }}
-      />
+      {links.map((link) => (
+        <a
+          key={link.href}
+          href={link.href}
+          style={{
+            position: "absolute",
+            left: link.left,
+            top: link.top,
+            width: "14%",
+            height: "9%",
+            zIndex: 10,
+            cursor: "pointer",
+          }}
+        />
+      ))}
     </main>
   );
 }
