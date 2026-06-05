92a4e34d59a05c84f44df8582bda717efec5d644
Touticimarket <contact@touticimarket.fr>
2026-06-05 16:33:27 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,50 @@
 export default function Home() {
   return (
-    <main>
+    <main style={{ position: "relative" }}>
       <img
         src="/public/banner-home.png"
         alt="TOUT ICI Market"
         style={{
           width: "100%",
+          display: "block",
+        }}
+      />
+
+      <a
+        href="/automobile"
+        title="Automobile"
+        style={{
+          position: "absolute",
+          left: "4%",
+          top: "41%",
+          width: "17%",
+          height: "8%",
+          display: "block",
+        }}
+      />
+
+      <a
+        href="/moto"
+        title="Moto"
+        style={{
+          position: "absolute",
+          left: "22%",
+          top: "41%",
+          width: "17%",
+          height: "8%",
+          display: "block",
+        }}
+      />
+
+      <a
+        href="/immobilier"
+        title="Immobilier"
+        style={{
+          position: "absolute",
+          left: "40%",
+          top: "41%",
+          width: "17%",
+          height: "8%",
           display: "block",
         }}
       />
