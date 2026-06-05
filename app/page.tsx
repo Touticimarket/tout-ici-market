2fa0afffcb66440a163512e90e810391eb218b58
Touticimarket <contact@touticimarket.fr>
2026-06-05 17:21:52 +0200

    Update link positions and dimensions in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 export default function Home() {
   const links = [
-    { href: "/automobile", left: "6%", top: "51%" },
-    { href: "/moto", left: "21%", top: "51%" },
-    { href: "/immobilier", left: "36%", top: "51%" },
-    { href: "/emploi", left: "51%", top: "51%" },
-    { href: "/telephones", left: "66%", top: "51%" },
+    { href: "/automobile", left: "3.3%", top: "44.1%", width: "18.7%" },
+    { href: "/moto", left: "22.4%", top: "44.1%", width: "16.8%" },
+    { href: "/immobilier", left: "39.6%", top: "44.1%", width: "17%" },
+    { href: "/emploi", left: "56.8%", top: "44.1%", width: "18.1%" },
+    { href: "/mode", left: "75.3%", top: "44.1%", width: "19.4%" },
 
-    { href: "/maison-jardin", left: "6%", top: "61%" },
-    { href: "/vacances-loisirs", left: "21%", top: "61%" },
-    { href: "/bricolage", left: "36%", top: "61%" },
-    { href: "/jeux-consoles", left: "51%", top: "61%" },
-    { href: "/autres", left: "66%", top: "61%" },
+    { href: "/telephones", left: "3.3%", top: "50.2%", width: "17.4%" },
+    { href: "/maison-jardin", left: "21.1%", top: "50.2%", width: "15.4%" },
+    { href: "/vacances-loisirs", left: "36.8%", top: "50.2%", width: "16.6%" },
+    { href: "/bricolage", left: "53.8%", top: "50.2%", width: "12%" },
+    { href: "/jeux-consoles", left: "66.1%", top: "50.2%", width: "14%" },
+    { href: "/autres", left: "80.3%", top: "50.2%", width: "14.4%" },
   ];
 
   return (
@@ -18,10 +19,7 @@ export default function Home() {
       <img
         src="/public/banner-home.png"
         alt="TOUT ICI Market"
-        style={{
-          width: "100%",
-          display: "block",
-        }}
+        style={{ width: "100%", display: "block" }}
       />
 
       {links.map((link) => (
@@ -32,8 +30,8 @@ export default function Home() {
             position: "absolute",
             left: link.left,
             top: link.top,
-            width: "14%",
-            height: "9%",
+            width: link.width,
+            height: "5.5%",
             zIndex: 10,
             cursor: "pointer",
           }}
